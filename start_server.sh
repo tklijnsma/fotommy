79ea71c9749c2deae27742a8195cd7241af9f6a3
Thomas Klijnsma <thomas.klijnsma@cern.ch>
2019-01-03 11:21:01 +0100

    Made startserver platform specific

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,7 @@
-cd /home/pi/code/flask/fotommy
-flask run --host 0.0.0.0 --port 23457 --with-threads
+if [[ $HOSTNAME =~ .*MacBook.* ]]; then
+    cd /Users/thomas/Work/flask/fotommy
+    flask run --port 5000
+else
+    cd /home/pi/code/flask/fotommy
+    flask run --host 0.0.0.0 --port 23457 --with-threads
+fi
